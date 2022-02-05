8d5aec603b01c9cd48c506eb8cd5d2e7e63c6f6b
Marat Kopytjuk <kopytjuk@gmail.com>
2022-02-05 21:04:55 +0100

    Continue writing blog.

--- scripts/blog.py
+++ scripts/blog.py
@@ -18,3 +18,39 @@ for i, (start, end) in enumerate(product(start_nodes, end_nodes)):
     path_as_ls = graph_path_to_linestring(traffic_lanes_graph, path)
     
     paths.append(DrivablePath(i, path_as_ls))
+
+
+
+# Frenet reference path
+@dataclass
+class DiscreteReferencePath:
+    """Represents a reference path by a collection of points sampled along the
+    arc length of the curve.
+    """
+    
+    points: np.ndarray  # in cartesian (metric) coordinates, as Nx3 (s, x, y)
+    spatial_resolution: float  # approximate
+    
+    def to_frenet(self, pt: Tuple[float, float]) -> Tuple[float, float]:
+        """Transform a cartesian coordinate to a Frenet coordinate.
+
+        Args:
+            pt (Tuple[float, float]): cartesian coordinate (x, y)
+
+        Returns:
+            Tuple[float, float]: frenet coordinate (s, d)
+        """
+        
+        pt = np.array(pt)
+        
+        # compute distances to each sample of the ref.-path
+        deltas = np.linalg.norm(self.points[:, 1:] - pt, axis=1)
+        
+        # take the index of the minium
+        idx_min = np.argmin(deltas)
+        
+        # arc length
+        s = self.points[idx_min, 0]
+        # perpendicular distance
+        d = deltas[idx_min]
+        return s,  d
